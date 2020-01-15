a652b7aaad1fd4f4e4acfca355889d44a490a5bb
Benjamín Arratia <benja@benjamins-mbp.lan>
2020-01-15 17:19:39 -0300

    test single image prediction

--- PredictImage.py
+++ PredictImage.py
@@ -5,10 +5,6 @@ from modeldef import *
 from PredictBoard import *
 
 
-checkpoint_path = 'weights.38-7.34.hdf5'
-IMG_PATH = '1B1b2KQ-1q1R1k2-kRRrP1Bq-pPq1Np1r-2bnQqpK-2Rb2KN-4b1B1-1n2qpP1-.png'
-
-
 def modelInit(checkpoint_path):
     model = create_model()
     model.load_weights(checkpoint_path)
@@ -24,8 +20,3 @@ def predictImg(model, IMG_PATH):
     y = model.predict(X)
     pred_fens = y_to_fens(y, 1)[0]
     return pred_fens
-
-
-#BASE_URL = "https://us-central1-spearsx.cloudfunctions.net/chesspic-fen-image/"
-#print("2D Prediction " + pred_fens)
-#display(SVG(url=BASE_URL+pred_fens))
---
+++ testSinglePrediction.py
@@ -0,0 +1,14 @@
+from PredictImage import *
+
+checkpoint_path = 'weights.38-7.34.hdf5'
+IMG_PATH = '1B1b2KQ-1q1R1k2-kRRrP1Bq-pPq1Np1r-2bnQqpK-2Rb2KN-4b1B1-1n2qpP1-.png'
+
+model = modelInit(checkpoint_path)
+y =  predictImg(model, IMG_PATH)
+
+print(y)
+
+#show image
+#BASE_URL = "https://us-central1-spearsx.cloudfunctions.net/chesspic-fen-image/"
+#print("2D Prediction " + pred_fens)
+#display(SVG(url=BASE_URL+pred_fens))
