6e7c3b0bfdaf783638b4eea87c45b1acda7423fd
baarratia <baarratia@uc.cl>
2020-01-15 18:16:05 -0500

    image resize

--- PredictPicture.py
+++ PredictPicture.py
@@ -3,14 +3,22 @@ from PredictImage import *
 checkpoint_path = 'weights.38-7.34.hdf5'
 model = modelInit(checkpoint_path)
 
+scale_percent = 47
+
 def show_webcam(mirror=False):
     cam = cv2.VideoCapture(0)
     while True:
         ret_val, img = cam.read()
         if mirror:
             img = cv2.flip(img, 1)
-        #cv2.imshow('raspicam', img)
-        y =  predictImg(model, img)
+        width = int(img.shape[1] * scale_percent / 100)
+        height = int(img.shape[0] * scale_percent / 100)
+        dim = (width, height)
+        resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+        crop = resized[0:224,38:262]
+        cv2.imshow('raspicam', crop)
+        print(crop.shape)
+        y =  predictImg(model, crop)
         print(y)
         if cv2.waitKey(1) == 27:
             break
