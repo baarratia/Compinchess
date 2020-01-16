3ca65a78c583e95e33746a5dab3b40858500ff66
baarratia <baarratia@uc.cl>
2020-01-16 10:46:14 -0500

    takepicture test

--- testcv.py
+++ testcv.py
@@ -1,18 +1,23 @@
 import cv2
 
 def show_webcam(mirror=False):
+    take = True
     cam = cv2.VideoCapture(0)
     while True:
         ret_val, img = cam.read()
         if mirror:
             img = cv2.flip(img, 1)
         cv2.imshow('raspicam', img)
+        if take is True:
+            cv2.imwrite('RNBKQBNR-PPPPPPPP-8-8-8-8-pppppppp-rkbkqbnr-.png', img)
+            take = False
+            print("saved!")
         if cv2.waitKey(1) == 27:
             break
     cv2.destroyAllWindows()
     
 def main():
-    show_webcam(mirror=True)
+    show_webcam()
     
 if __name__== '__main__':
     main()
