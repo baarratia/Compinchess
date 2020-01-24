57ed09eae7269dd35989fe31c1c8d70f6ab8f156
Benjamín Arratia <benja@benjamins-mbp.lan>
2020-01-24 14:51:01 -0300

    ScaleReverse

--- PredictPicture.py
+++ PredictPicture.py
@@ -3,7 +3,7 @@ from PredictImage import *
 checkpoint_path = 'model_brown_60.h5'
 model = modelInit(checkpoint_path)
 
-#scale_percent = 47
+scale_percent = 47
 
 def show_webcam(mirror=False):
     cam = cv2.VideoCapture(0)
@@ -11,14 +11,14 @@ def show_webcam(mirror=False):
         ret_val, img = cam.read()
         if mirror:
             img = cv2.flip(img, 1)
-        #width = int(img.shape[1] * scale_percent / 100)
-        #height = int(img.shape[0] * scale_percent / 100)
-        #dim = (width, height)
-        #resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-        #crop = resized[0:224,38:262]
-        cv2.imshow('raspicam', img)
-        #print(crop.shape)
-        y =  predictImg(model, img)
+        width = int(img.shape[1] * scale_percent / 100)
+        height = int(img.shape[0] * scale_percent / 100)
+        dim = (width, height)
+        resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+        crop = resized[0:224,38:262]
+        cv2.imshow('raspicam', crop)
+        print(crop.shape)
+        y =  predictImg(model, crop)
         print(y)
         if cv2.waitKey(1) == 27:
             break
